f146f92cc4360ca09598b4d4e833238906784931
Mina Nabil <minanabil2004@gmail.com>
2024-12-08 16:23:00 +0200

    deleting the old server

--- Backend/src/main/java/com/WebMail/WebMail/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Webmail.Webmail.repository;
-
-import com.Webmail.Webmail.service.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User,Long> {
-    User findByEmailOrUsername(String email, String username);
-}
