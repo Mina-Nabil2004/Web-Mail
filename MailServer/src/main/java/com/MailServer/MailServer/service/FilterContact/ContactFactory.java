a7d039c0120b760f34718de844fc2729e67e7745
Boudynab <legendboudy@gmail.com>
2024-12-13 05:17:45 +0200

    merging

--- MailServer/src/main/java/com/MailServer/MailServer/service/FilterContact/ContactFactory.java
+++ MailServer/src/main/java/com/MailServer/MailServer/service/FilterContact/ContactFactory.java
@@ -1,19 +1,19 @@
-package com.MailServer.MailServer.service.FilterContact;
-
-public class ContactFactory {
-    public static CriteriaContact getCriteria(ContactFilterDTO DTO, String criteria){
-        if(criteria==null){
-            return null;
-      }
-//        else if(criteria.equals("email")){
-//            return new CriteriaEmail(DTO);
+//package com.MailServer.MailServer.service.FilterContact;
+//
+//public class ContactFactory {
+//    public static CriteriaContact getCriteria(ContactFilterDTO DTO, String criteria){
+//        if(criteria==null){
+//            return null;
+//      }
+////        else if(criteria.equals("email")){
+////            return new CriteriaEmail(DTO);
+////        }
+//        else if(criteria.equals("name")){
+//            return new CriteriaName(DTO);
 //        }
-        else if(criteria.equals("name")){
-            return new CriteriaName(DTO);
-        }
-        else if(criteria.equals("show all")){
-            return new OrCriteria(DTO);
-        }
-        return null;
-    }
-}
+//        else if(criteria.equals("show all")){
+//            return new OrCriteria(DTO);
+//        }
+//        return null;
+//    }
+//}
